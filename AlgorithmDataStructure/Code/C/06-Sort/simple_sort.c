5409e5f82c28f8d9eed8f39a86bdf473c96a8e84
MA806P <mayp126@126.com>
2019-05-22 12:11:55 +0800

    add c sort code

---
+++ AlgorithmDataStructure/Code/C/06-Sort/simple_sort.c
@@ -0,0 +1,172 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
+#define SWAP(a, b) \
+do { \
+(a) ^= (b);\
+(b) ^= (a);\
+(a) ^= (b);\
+} while(0)
+
+
+void dump(int a[], int size) {
+    if (size <= 0) {
+        return;
+    }
+    int i = 0;
+    for (i = 0; i < size; i++) {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+    printf("\n");
+    return;
+}
+
+void bubble_sort(int a[], int size) {
+    if (size <= 1) { return; }
+    int i = 0, j = 0, swap_flag = 0;
+    for (i = size - 1; i > 0; i--) {
+        swap_flag = 0;
+        for (j = 0; j < i; j++) {
+            if (a[j] > a[j + 1]) {
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+                
+                //SWAP(a[j], a[j + 1]);
+                swap_flag = 1;
+            }
+        }
+        if (swap_flag == 0) { break; }
+    }
+    return;
+}
+
+void my_bubble_sort(int a[], int size) {
+    if (size <= 1) { return; }
+    int i = 0, j = 0, swap_flag = 0;
+    for (i = 0; i < size; i++) {
+        swap_flag = 0;
+        for (j = 0; j < size - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                SWAP(a[j], a[j + 1]);
+                swap_flag = 1;
+            }
+        }
+        if (swap_flag == 0) { break; }
+    }
+    return;
+}
+
+void insert_sort(int a[], int size) {
+    if (size <= 1) { return; }
+    int i = 0, j = 0, key = 0;
+    for (i = 1; i < size; i++) {
+        key = a[i];
+        // i 之前的都是有序的，找到比 i 位的值小的插入到后面
+        // 比 i 位值大的，往后移一位
+        for (j = i - 1; j >= 0; j--) {
+            if (a[j] > key) {
+                a[j + 1] = a[j];
+            } else {
+                break;
+            }
+        }
+        //j = i - 1;
+        //while (j >= 0 && a[j] > key) { a[j + 1] = a[j]; j--; }
+        
+        a[j + 1] = key;
+    }
+    return;
+}
+
+void select_sort(int a[], int size) {
+    if (size <= 1) {
+        return;
+    }
+    //已排序区，未排序区
+    //在未排序区找到最小的，插入到已排序区的最后
+    int i = 0, j = 0, min = 0;
+    for (i  = 0; i < size - 1; i++) {
+        min = i;
+        for (j = i + 1; j < size; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            SWAP(a[i], a[min]);
+        }
+    }
+    return;
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    //int a[10] = {7,5,6,8,1,2,3,4,9,0};
+    //int size = sizeof(a)/sizeof(a[0]);
+    //bubble_sort(a, size);
+    //insert_sort(a, size);
+    //select_sort(a, size);
+    
+    //my_bubble_sort(a, size);
+    
+    //dump(a, size);
+    
+    
+    int testa1[200][2000], testa2[200][2000], testa3[200][2000];
+    int i = 0, j = 0;
+    for (i = 0; i < 200; i++) {
+        for (j = 0; j < 2000; j++) {
+            int value = arc4random() % 100000;
+            testa1[i][j] = value + 1;
+            testa2[i][j] = value + 2;
+            testa3[i][j] = value + 3;
+        }
+    }
+    
+    
+    
+    clock_t start1, end1;
+    double cpu_time_used;
+    start1 = clock();
+    for (i = 0; i < 200; i++) {
+        bubble_sort(testa1[i], 2000);
+    }
+    end1 = clock();
+    cpu_time_used = ((double) (end1 - start1)) / CLOCKS_PER_SEC;
+    printf("---------1 = %.8lf\n", cpu_time_used);
+    
+    
+    start1 = clock();
+    for (i = 0; i < 200; i++) {
+        insert_sort(testa2[i], 2000);
+    }
+    end1 = clock();
+    cpu_time_used = ((double) (end1 - start1)) / CLOCKS_PER_SEC;
+    printf("---------2 = %.8lf\n", cpu_time_used);
+    
+    
+    start1 = clock();
+    for (i = 0; i < 200; i++) {
+        select_sort(testa3[i], 2000);
+    }
+    end1 = clock();
+    cpu_time_used = ((double) (end1 - start1)) / CLOCKS_PER_SEC;
+    printf("---------3 = %.8lf\n", cpu_time_used);
+    
+    
+    //---------1 = 1.53191600
+    //---------2 = 0.54968900
+    //---------3 = 0.87126800
+    
+    return 0;
+}
+
+
