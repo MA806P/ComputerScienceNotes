d17e9b51d77f3b8d14969a5fbeb41f0455b6e6bb
MA806P <mayp126@126.com>
2019-10-30 23:20:52 +0800

    add leetcode problem

---
+++ AlgorithmDataStructure/LeetCode/1-TwoSum/sum.swift
@@ -0,0 +1,33 @@
+
+
+class Solution {
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        for (i, num) in nums.enumerated() {
+            if let lastIndex = dict[target - num] {
+                return [lastIndex, i]
+            } else {
+                dict[num] = i
+            }
+        }
+        fatalError("no")
+    }
+    
+    func twoSum(nums: [Int], _ target: Int) -> Bool {
+        var set = Set<Int>()
+        for num in nums {
+            if set.contains(target - num) {
+                return true
+            }
+            set.insert(num)
+        }
+        return false
+    }
+
+    
+    //let array = [1,3,4,5,6,7,8,9]
+    //twoSum(nums: array, 10) //true
+    //twoSumIndex(nums: array, target: 10) // [2,4]
+
+}
