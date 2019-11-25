edc8cd9d43a68d53635622843d9d736baf734cd9
MA806P <mayp126@126.com>
2019-11-25 18:10:02 +0800

    update

--- AlgorithmDataStructure/LeetCode/1-TwoSum/sum.swift
+++ AlgorithmDataStructure/LeetCode/1-TwoSum/sum.swift
@@ -2,7 +2,7 @@
 
 class Solution {
     
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    func twoSumIndex(_ nums: [Int], _ target: Int) -> [Int] {
         var dict = [Int: Int]()
         for (i, num) in nums.enumerated() {
             if let lastIndex = dict[target - num] {
